e1e484ae7333323861bca2e2b84edfa8c3b524fb
Benjamin Leeds <baleeds@ncsu.edu>
2020-02-06 21:54:55 -0500

    add: Favorite scope and field on annotation model

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -4,5 +4,14 @@ class Annotation < ApplicationRecord
   validates :text, presence: true
   validates :verse_id, presence: true
 
+  has_many :user_annotation_favorites
+  has_many :favorited_by, through: :user_annotation_favorites, source: :user, dependent: :destroy
+
   belongs_to :user
+
+  scope :favorite_for_user, ->(user) { joins(:user_annotation_favorites).where("user_annotation_favorites.user_id = #{user.id}") }
+
+  def favorited?(user)
+    user_annotation_favorites.any? { |user_annotation_favorite| user_annotation_favorite.user_id == user.id }
+  end
 end
--- spec/models/annotation_spec.rb
+++ spec/models/annotation_spec.rb
@@ -6,4 +6,41 @@ RSpec.describe Annotation, type: :model do
       expect(build_stubbed(:annotation)).to be_valid
     end
   end
+
+  describe ".favorite_for_user" do
+    it "returns annotations favorited by that user" do
+      annotation = create(:annotation)
+      user = create(:user)
+
+      create(:user_annotation_favorite, annotation: annotation, user: user)
+      different_favorited_annotation = create(:user_annotation_favorite)
+
+      annotations = Annotation.favorite_for_user(user)
+
+      expect(annotations).to_not include(different_favorited_annotation)
+      expect(annotations.count).to be(1)
+      expect(annotations.first).to eq(annotation)
+    end
+  end
+
+  describe "#favorited?" do
+    it "returns true when a annotation is favorited" do
+      annotation = create(:annotation)
+      user = create(:user)
+      create(:user_annotation_favorite, user: user, annotation: annotation)
+
+      result = annotation.favorited?(user)
+
+      expect(result).to be(true)
+    end
+
+    it "returns false when a annotation is favorited" do
+      annotation = create(:annotation)
+      user = create(:user)
+
+      result = annotation.favorited?(user)
+
+      expect(result).to be(false)
+    end
+  end
 end
