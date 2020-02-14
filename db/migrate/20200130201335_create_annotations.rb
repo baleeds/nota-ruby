f9057dbfa659a3c95f72f24149e90dba8765a961
Benjamin Leeds <baleeds@ncsu.edu>
2020-02-14 14:59:38 -0500

    fix: Seeds and migrations

--- db/migrate/20200130201335_create_annotations.rb
+++ db/migrate/20200130201335_create_annotations.rb
@@ -2,7 +2,7 @@ class CreateAnnotations < ActiveRecord::Migration[6.0]
   def change
     create_table :annotations do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :verse, null: false, foreign_key: true
+      t.references :verse, null: false, foreign_key: true, type: :string
       t.string :text, null: false
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,54 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_015435) do
+ActiveRecord::Schema.define(version: 2020_02_07_015435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "annotations", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.string "text", null: false
     t.string "verse_id", null: false
+    t.string "text", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_annotations_on_user_id"
-  end
-
-  create_table "books", force: :cascade do |t|
-    t.string "title", null: false
-    t.string "authors"
-    t.string "description"
-    t.string "image_url"
-    t.string "publisher"
-    t.string "isbn"
-    t.datetime "lost_at"
-    t.datetime "removed_at"
-    t.integer "page_count"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "rental_id"
-    t.index ["rental_id"], name: "index_books_on_rental_id"
-  end
-
-  create_table "favorite_books", force: :cascade do |t|
-    t.bigint "book_id"
-    t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["book_id", "user_id"], name: "index_favorite_books_on_book_id_and_user_id", unique: true
-    t.index ["book_id"], name: "index_favorite_books_on_book_id"
-    t.index ["user_id"], name: "index_favorite_books_on_user_id"
-  end
-
-  create_table "rentals", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "book_id", null: false
-    t.datetime "returned_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["book_id"], name: "index_rentals_on_book_id"
-    t.index ["user_id"], name: "index_rentals_on_user_id"
+    t.index ["verse_id"], name: "index_annotations_on_verse_id"
   end
 
   create_table "reset_password_tokens", force: :cascade do |t|
@@ -80,16 +45,6 @@ ActiveRecord::Schema.define(version: 2020_02_08_015435) do
     t.index ["user_id"], name: "index_user_annotation_favorites_on_user_id"
   end
 
-  create_table "user_book_ratings", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "book_id", null: false
-    t.integer "rating"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["book_id"], name: "index_user_book_ratings_on_book_id"
-    t.index ["user_id"], name: "index_user_book_ratings_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest", null: false
@@ -110,14 +65,8 @@ ActiveRecord::Schema.define(version: 2020_02_08_015435) do
   end
 
   add_foreign_key "annotations", "users"
-  add_foreign_key "books", "rentals"
-  add_foreign_key "favorite_books", "books"
-  add_foreign_key "favorite_books", "users"
-  add_foreign_key "rentals", "books"
-  add_foreign_key "rentals", "users"
+  add_foreign_key "annotations", "verses"
   add_foreign_key "reset_password_tokens", "users"
   add_foreign_key "user_annotation_favorites", "annotations"
   add_foreign_key "user_annotation_favorites", "users"
-  add_foreign_key "user_book_ratings", "books"
-  add_foreign_key "user_book_ratings", "users"
 end
