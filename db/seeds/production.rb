c371624a8975d30ff8319e05063afafc2a341ec4
Benjamin Leeds <bleeds@samaritan.org>
2020-10-20 12:20:34 -0400

    add: Full verse seeds for production

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -19,7 +19,7 @@ erica = User.create(
 csv_text = File.read(Rails.root.join('db', 'assets', 't_web.csv'))
 csv = CSV.parse(csv_text, headers: true, encoding: 'ISO-8859-1')
 # Only take the first 100 for testing purposes
-csv[0..100].each do |row|
+csv.each do |row|
   t = Verse.new
   t.id = row['id']
   t.book_number = row['b']
